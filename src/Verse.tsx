ed41a9a7be25747f69b3670c514d5758b55a20fa
Dan Seethaler <dan@seethalers.com>
2022-06-04 16:11:41 -0600

    Move copy to dedicated button

--- src/Verse.tsx
+++ src/Verse.tsx
@@ -24,65 +24,109 @@ const Verse = ({ verse, searchString }: Props) => {
         padding: 16,
       }}
     >
-      <Pressable onPress={() => Clipboard.setString(buildCopyText(verse))}>
-        <Text
-          style={{
-            color: colors.text,
-            fontWeight: "800",
-          }}
-        >
-          {verse.reference}
-        </Text>
-        <Text
-          style={{
-            color: colors.text,
-            paddingVertical: 8,
-            fontSize: 16,
-            lineHeight: 22,
-          }}
-        >
-          {verseParts.map((textPart, index) => {
-            if (verseParts.length - 1 === index) {
-              return textPart;
-            }
-
-            return (
-              <React.Fragment key={index}>
-                {textPart}
-                <Text
-                  style={{
-                    color: colors.background,
-                    backgroundColor: colors.linkBackground,
-                    fontWeight: "bold",
-                    borderRadius: 3,
-                    overflow: "hidden",
-                  }}
-                >
-                  {searchString.toLowerCase()}
-                </Text>
-              </React.Fragment>
-            );
-          })}
-        </Text>
-      </Pressable>
-      <Pressable
+      <Text
         style={{
-          marginVertical: 8,
-          borderRadius: 8,
-          padding: 16,
-          paddingVertical: 12,
-          backgroundColor: colors.background,
-          justifyContent: "space-between",
-          flexDirection: "row",
-          alignItems: "center",
+          color: colors.text,
+          fontWeight: "800",
         }}
-        onPress={() => Linking.openURL(buildUrl(verse))}
       >
-        <Text style={{ color: colors.linkBackground, fontWeight: "600" }}>
-          Open
-        </Text>
-        <Feather name="external-link" size={18} color={colors.linkBackground} />
-      </Pressable>
+        {verse.reference}
+      </Text>
+      <Text
+        style={{
+          color: colors.text,
+          paddingVertical: 8,
+          fontSize: 16,
+          lineHeight: 22,
+        }}
+      >
+        {verseParts.map((textPart, index) => {
+          if (verseParts.length - 1 === index) {
+            return textPart;
+          }
+
+          return (
+            <React.Fragment key={index}>
+              {textPart}
+              <Text
+                style={{
+                  color: colors.background,
+                  backgroundColor: colors.linkBackground,
+                  fontWeight: "bold",
+                  borderRadius: 3,
+                  overflow: "hidden",
+                }}
+              >
+                {searchString.toLowerCase()}
+              </Text>
+            </React.Fragment>
+          );
+        })}
+      </Text>
+      <View
+        style={{
+          flexDirection: "row",
+          alignSelf: "stretch",
+          alignItems: "stretch",
+        }}
+      >
+        <Pressable
+          style={{
+            marginVertical: 8,
+            borderTopLeftRadius: 8,
+            borderBottomLeftRadius: 8,
+            padding: 16,
+            paddingVertical: 12,
+            backgroundColor: colors.background,
+            flexDirection: "row",
+            alignItems: "center",
+            flex: 1,
+            marginRight: 1,
+          }}
+          onPress={() => Clipboard.setStringAsync(buildCopyText(verse))}
+        >
+          <Feather name="copy" size={18} color={colors.linkBackground} />
+          <Text
+            style={{
+              marginLeft: 8,
+              color: colors.linkBackground,
+              fontWeight: "600",
+            }}
+          >
+            Copy
+          </Text>
+        </Pressable>
+        <Pressable
+          style={{
+            marginVertical: 8,
+            borderTopRightRadius: 8,
+            borderBottomRightRadius: 8,
+            padding: 16,
+            paddingVertical: 12,
+            backgroundColor: colors.background,
+            flexDirection: "row",
+            alignItems: "center",
+            flex: 1,
+            marginLeft: 1,
+          }}
+          onPress={() => Linking.openURL(buildUrl(verse))}
+        >
+          <Feather
+            name="external-link"
+            size={18}
+            color={colors.linkBackground}
+          />
+          <Text
+            style={{
+              marginLeft: 8,
+              color: colors.linkBackground,
+              fontWeight: "600",
+            }}
+          >
+            Open
+          </Text>
+        </Pressable>
+      </View>
     </View>
   );
 };
